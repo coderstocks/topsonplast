c88199532d9aac7ea642813bac3de5ecfe912fb9
Viral Ads Media <info.viraladsmedia@gmail.com>
2024-11-23 15:17:04 +0530

    SDFSDF

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// slick
+$('.review').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="bi bi-arrow-left"></i></button>',
+  nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="bi bi-arrow-right"></i></button>',
+  speed: 200,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+
+
 // JavaScript for Cursor Effect
 const cursor = document.querySelector(".custom-cursor");
 
